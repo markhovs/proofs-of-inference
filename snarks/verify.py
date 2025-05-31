17e2a6f69fa81fcd3f3157f8a2c8db148ddb2402
benbencik <bencik42@gmail.com>
2025-05-31 20:55:58 +0200

    cleanup

--- snarks/verify.py
+++ snarks/verify.py
@@ -10,29 +10,14 @@ def main():
         required=True,
         help="Model name, used to locate files under models/<model>-..."
     )
-    parser.add_argument(
-        "--proof-path",
-        type=str,
-        help="Path to the proof file",
-    )
+    
     args = parser.parse_args()
     model_base = f"models/{args.model}"
-    proof_path = f"{args.proof_path}"
-    
+    proof_path = "proof.json"
     
     proof_path = os.path.join(proof_path)
     settings_path = os.path.join(f'{model_base}-settings.json')
     vk_path = os.path.join(f'{model_base}-test.vk')
-
-    print(proof_path)
-    print(settings_path)
-    #  verify each file
-    if not os.path.exists(proof_path):
-        raise FileNotFoundError(f"Proof file {proof_path} does not exist.")
-    if not os.path.exists(settings_path):
-        raise FileNotFoundError(f"Settings file {settings_path} does not exist.")
-    if not os.path.exists(vk_path):
-        raise FileNotFoundError(f"Verification key file {vk_path} does not exist.")
 
     res = ezkl.verify(proof_path, settings_path, vk_path)
 
